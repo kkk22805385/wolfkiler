a86e341baf06c745381bf0cb688ce87f3aacc845
Bruce <buttrtlionprince@gmail.com>
2020-02-14 16:33:32 +0800

    nice

--- wolfkiler/okViewController.swift
+++ wolfkiler/okViewController.swift
@@ -14,6 +14,7 @@ class okViewController: UIViewController {
         super.viewDidLoad()
 print(12223)
         print(222222)
+        print(33456)
         // Do any additional setup after loading the view.
     }
     
